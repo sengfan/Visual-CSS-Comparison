41b3b5b66056f03cf4d0d7218f235185f71d117e
Zhou Fang <zfang@crateandbarrel.com>
2019-08-06 12:47:42 -0500

    remove example settings

--- example/QaDemo.ts
+++
@@ -1,5 +0,0 @@
-import { VisualCssComparison } from '../src/VisualCssComparison';
-import { config } from './config';
-
-const exampleProgress = new VisualCssComparison(config);
-exampleProgress.run();
